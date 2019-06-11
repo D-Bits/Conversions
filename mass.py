d3d781a3061ae6c14808b5a195a209871a2c9d12
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-06-11 12:49:54 -0700

    Rounded masses to  2 digits

--- mass.py
+++ mass.py
@@ -4,10 +4,12 @@ def pounds_to_kilograms():
     pounds = float(input('Enter an amount in pounds to be converted to kilos: '))
     kilos = pounds / 2.2046
 
-    print(kilos)
+    print(round(kilos, 2))
 
 
 def kilograms_to_pounds():
 
     kilos = float(input('Enter an amount in pounds to be converted to kilos: '))
     pounds = kilos * 2.2046
+
+    print(round(pounds, 2))
