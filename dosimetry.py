45dd740a94c786d57e95e5b855e86d11f48f825a
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-06-12 13:46:25 -0700

    Added f-strings

--- dosimetry.py
+++ dosimetry.py
@@ -8,20 +8,23 @@ dosimetry_options = {
     '2': 'Sieverts to Rems'
 }
 
+
 def rems_to_sieverts():
 
     rems = float(input('Enter an amount of Roentigen Equivalent Mans (rems) to be converted to Sieverts: '))
     sieverts = rems / 100
+    rounded = round(sieverts, 2)
 
-    print(round(sieverts, 2))
+    print(f'{rems} rems equals {rounded} Sv.')
 
 
 def sieverts_to_rems():
 
     sieverts = float(input('Enter an amount of Sieverts to be converted to Roentigen Equivalent Mans (rems): '))
     rems = sieverts * 100
+    rounded = round(rems, 2)
 
-    print(round(rems, 2))
+    print(f'{sieverts} Sv equals {rounded} rems.')
 
 
 def dosimetry_choices():
