07d6159a887513d6b80c8abb1d6d89e80f69cefc
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-06-12 16:07:33 -0700

    Added f-strings

--- temperature.py
+++ temperature.py
@@ -13,48 +13,54 @@ def fahrenheit_to_celsius():
 
     fahrenheit = float(input('Enter an amount in fahrenheit to be converted to celsius: '))
     celsius = fahrenheit - 32 / 1.80
+    rounded = round(celsius, 2)
 
-    print(round(celsius, 2))
+    print(f'{fahrenheit} degrees F equals {rounded} degrees C.')
 
 
 def celsius_to_fahrenheit():
 
     celsius = float(input('Enter an amount in celsius to be converted to fahrenheit: '))
     fahrenheit = celsius * 1.80 + 32.00
+    rounded = round(fahrenheit, 2)
 
-    print(round(fahrenheit, 2))
+    print(f'{celsius} degrees C equals {rounded} degrees F.')
     
 
 def celsius_to_kelvin():
     
     celsius = float(input('Enter an amount in celsius to be converted to Kelvin: '))
     kelvin = celsius + 273.15
+    rounded = round(kelvin, 2)
 
-    print(round(kelvin, 2))
+    print(f'{celsius} degrees C equals {rounded} K.')
 
 
 def kelvin_to_celsius():
     
     kelvin = float(input('Enter an amount in Kelvin to be converted to celsius: '))
     celsius = kelvin - 273.15
+    rounded = round(celsius, 2)
 
-    print(round(celsius, 2))
+    print(f'{kelvin} degrees K equals {rounded} degrees C.')
 
 
 def fahrenheit_to_kelvin():
 
     fahrenheit = float(input('Enter an amount in Kelvin to be converted to fahrenheit: '))
     kelvin = fahrenheit - 32 / 1.80 + 273.15
+    rounded = round(kelvin, 2)
 
-    print(round(kelvin, 2))
+    print(f'{fahrenheit} degrees F equals {rounded} degrees K.')
 
 
 def kelvin_to_fahrenheit():
 
     kelvin = float(input('Enter an amount in fahrenheit to be converted to Kelvin: '))
     fahrenheit = (kelvin - 273.15) * 1.80 + 32
+    rounded = round(fahrenheit, 2)
 
-    print(round(fahrenheit, 2))
+    print(f'{kelvin} degrees K equals {rounded} degrees F.')
 
 
 def temp_choices():
