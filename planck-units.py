01d7d886e5fd364b6b184d42a12b4a5927affdd3
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-06-12 16:09:30 -0700

    Rounded off results, added f-strings.

--- planck-units.py
+++ planck-units.py
@@ -4,6 +4,7 @@ def meters_to_planck_lengths():
     meters = float(input('Enter an amount of meters to be converted into Planck Lengths: '))
     conversion = 1.16 * 10**-34
     planck_lengths = meters / conversion
+    rounded = round(planck_lengths, 2)
 
-    print(round(planck_lengths, 2))
+    print(f'{meters} meters equals {rounded} Planck lengths.')
 
