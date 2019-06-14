090e42682b2c013a9120be0a929c4214b362b810
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-06-13 23:18:36 -0700

    Added grays to rads, rads to grays

--- dosimetry.py
+++ dosimetry.py
@@ -5,10 +5,11 @@ measurements of ionizing radiation (Rems, Sieverts, etc)
 
 dosimetry_options = {
     '1': 'Rems to Sieverts',
-    '2': 'Sieverts to Rems'
+    '2': 'Sieverts to Rems',
+    '3': 'Rads to Grays'
 }
 
-
+# Rems and Sieverts both measure doseage equivalents
 def rems_to_sieverts():
 
     rems = float(input('Enter an amount of Roentigen Equivalent Mans (rems) to be converted to Sieverts: '))
@@ -27,6 +28,26 @@ def sieverts_to_rems():
     print(f'{sieverts} Sv equals {rounded} rems.')
 
 
+# Grays and rads measure Absorbed doses
+def rads_to_grays():
+
+    rads = float(input('Enter an amount of rads to be converted to grays: '))
+    grays = rads / 100
+    rounded = round(grays, 2)
+
+    print(f'{rads} rads equals {rounded} grays.')
+
+
+def rads_to_grays():
+
+    grays = float(input('Enter an amount of grays to be converted to rads: '))
+    rads = grays * 100
+    rounded = round(rads, 2)
+
+    print(f'{grays} grays equals {rounded} rads.')
+
+
+# Display the available unit conversions, prompt the user to choose
 def dosimetry_choices():
 
     print()
@@ -36,12 +57,14 @@ def dosimetry_choices():
         print(key, val)
 
     print()
-    conversion = int(input('Enter one of the above integers to specify a mass conversion: '))
+    conversion = int(input('Enter one of the above integers to specify a dosimetry conversion: '))
     print()
 
     if conversion == 1:
         rems_to_sieverts()
     elif conversion == 2:
         sieverts_to_rems()
+    elif conversion == 3:
+        rads_to_grays()
     else:
         raise Exception('Invalid value entered.')
