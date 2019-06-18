f4d868b92e11617b97344dcb226df60731b73255
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-06-17 19:17:00 -0700

    Prevented user from entering a value < absolute 0

--- temperature.py
+++ temperature.py
@@ -12,6 +12,8 @@ temp_options = {
 def fahrenheit_to_celsius():
 
     fahrenheit = float(input('Enter an amount in fahrenheit to be converted to celsius: '))
+    if fahrenheit < -459.67:
+        raise Exception('Cannot be below absolute zero!')
     celsius = (fahrenheit -32) / 1.80
     rounded = round(celsius, 2)
 
@@ -21,6 +23,8 @@ def fahrenheit_to_celsius():
 def celsius_to_fahrenheit():
 
     celsius = float(input('Enter an amount in celsius to be converted to fahrenheit: '))
+    if celsius < -273.15:
+        raise Exception('Cannot be below absolute zero!')
     fahrenheit = celsius * 1.80 + 32.00
     rounded = round(fahrenheit, 2)
 
@@ -30,6 +34,8 @@ def celsius_to_fahrenheit():
 def celsius_to_kelvin():
     
     celsius = float(input('Enter an amount in celsius to be converted to Kelvin: '))
+    if celsius < -273.15:
+        raise Exception('Cannot be below absolute zero!')
     kelvin = celsius + 273.15
     rounded = round(kelvin, 2)
 
@@ -39,6 +45,8 @@ def celsius_to_kelvin():
 def kelvin_to_celsius():
     
     kelvin = float(input('Enter an amount in Kelvin to be converted to celsius: '))
+    if kelvin < 0:
+        raise Exception('Cannot be below absolute zero!')
     celsius = kelvin - 273.15
     rounded = round(celsius, 2)
 
@@ -48,6 +56,8 @@ def kelvin_to_celsius():
 def fahrenheit_to_kelvin():
 
     fahrenheit = float(input('Enter an amount in fahrenheit to be converted to Kelvin: '))
+    if fahrenheit < -459.67:
+        raise Exception('Cannot be below absolute zero!')
     kelvin = ((fahrenheit - 32) / 1.80)+ 273.15
     rounded = round(kelvin, 2)
 
@@ -57,6 +67,8 @@ def fahrenheit_to_kelvin():
 def kelvin_to_fahrenheit():
 
     kelvin = float(input('Enter an amount in fahrenheit to be converted to Kelvin: '))
+    if kelvin < 0:
+        raise Exception('Cannot be below absolute zero!')
     fahrenheit = (kelvin - 273.15) * 1.80 + 32
     rounded = round(fahrenheit, 2)
 
